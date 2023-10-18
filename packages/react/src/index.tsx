3eb4a8e9590855e9d39f52218fd58638019e4d20
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-10-18 00:04:59 -0300

    feat: export Button component with variants

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,13 +1,31 @@
-import { colors } from '@ignite-ui/tokens'
-
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$ignite500',
+  backgroundColor: '$ignite300',
   borderRadius: '$md',
+  padding: '$4',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button style={{ color: colors.ignite300 }}>Hello World</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
