7f0e6a957f36e7aa34c3c06fdf3a1aa420911828
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-10-26 23:21:42 -0300

    feat: change packages names due changesets publishing

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonProps, Text } from '@ignite-ui/react'
+import { Button, ButtonProps, Text } from '@zizao/react'
 import { Meta, StoryObj } from '@storybook/react'
 import { ArrowRight } from 'phosphor-react'
 
