9059239b22a5274f7941675f7a2fd5930b9f7f9d
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-10-26 22:53:25 -0300

    feat: add subpath to storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -30,6 +30,13 @@ const config = {
   },
   core: {
     "builder": "@storybook/builder-vite"
+  },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/ignite-design-system/'
+    }
+
+    return config
   }
 };
 export default config;
